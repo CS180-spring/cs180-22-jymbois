3dbc8a268edcd9028ed95c11f198819df5ab6608
Victor <87928341+VMSandoval25@users.noreply.github.com>
2023-04-21 11:58:36 -0700

    adding a register page

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,10 +1,14 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from 'react-native';
+import React from 'react';
+import { Button } from 'react-native';
+import { useNavigation } from '@react-navigation/core';
 
 const LoginScreen = () => {
+  const navigation = useNavigation();
   return (
     <View>
       <Text>I am the LoginScreen</Text>
+      <Button title="Go to Calender" onPress={() => navigation.navigate('Register') }/>
     </View>
   );
 };
