28347de145e85ee973c8204b6366388ec75ea64b
Victor <87928341+VMSandoval25@users.noreply.github.com>
2023-04-22 01:01:34 -0700

    s

--- hooks/useAuth.js
+++ hooks/useAuth.js
@@ -1,10 +1,14 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import React, { createContext } from 'react'
 
-export const AuthProvider = () => {
+const AuthContext = createContext({
+  // initial state
+})
+
+export const AuthProvider = ({ children }) => {
   return (
-    <View>
-      <Text>useAuth</Text>
-    </View>
+    <AuthContext.Provider value={null}>
+      {children}
+    </AuthContext.Provider>
   );
 };
