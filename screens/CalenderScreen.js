27a89a7808be173ac0c6604277d8d008f611ea6b
boney171 <ttran456@ucr.edu>
2023-05-19 23:34:00 -0700

    Finished remodeling the calendar page

--- screens/CalenderScreen.js
+++ screens/CalenderScreen.js
@@ -161,34 +161,45 @@ const CalenderScreen = () => {
  
   return (
     
-    <ScrollView >  
-    <Text style={styles.titleText}>Select the date to record your work out gym rat!</Text>
+    <ScrollView  style={styles.body}>  
       <Calendar 
         style={styles.calendar} 
         onDayPress={ handleDayPress}
         minDate={"2023-04-01"}
-        maxDate={"2023-12-31"}
+        maxDate={"2099-12-31"}
         hideExtraDays={true}
         markedDates={{
           [recordDate]: {
             selected: true,
-            selectedColor: 'tan', // or any color you want for the selected date
+            selectedColor: 'gray', // or any color you want for the selected date
           }
         }}
+        theme={{
+          textSectionTitleColor: 'black', // Changes the color of weekday names (Mon, Tue, etc.)
+          monthTextColor: 'black', // Changes the color of the month name
+          textMonthFontWeight: 'bold',  // Makes month name bold
+          textMonthFontSize: 16, // Makes month name font size larger
+          arrowColor: 'gray',
+          todayTextColor: 'brown', // Changes the color of the current day
+        }}
       />
+      
+      <Text style={styles.titleText}>Select a date:</Text>
 
-      <View style={styles.buttons}>
+      <View style={styles.calendarButtons}>
         <TouchableOpacity 
 
           onPress={selectViewExerciseRecord}
-          style={styles.button3}> 
-        <Text style={ styles.buttonText }>View Exercise Record</Text>
+          style={styles.viewERButton}> 
+        <Text style={ styles.calendarButtonTextTop }>View</Text>
+        <Text style={ styles.calendarButtonTextBottom }>Exercise Record</Text>
         </TouchableOpacity>
 
         <TouchableOpacity 
           onPress={selectInsertExerciseRecord}
-          style={styles.button3}> 
-          <Text style={ styles.buttonText }>Insert Exercise Record</Text>
+          style={styles.insertERButton}> 
+          <Text style={ styles.calendarButtonTextTop }>Insert</Text>
+          <Text style={ styles.calendarButtonTextBottom }>Exercise Record</Text>
         </TouchableOpacity>
       
       </View>
@@ -283,6 +294,9 @@ const CalenderScreen = () => {
   );
 }
 const styles = StyleSheet.create({
+  body: {
+    backgroundColor: 'white',
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
@@ -306,17 +320,37 @@ const styles = StyleSheet.create({
     width: 100,
     alignItems: 'center'
   },
+
+  calendarButtons:{
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    maxWidth: '100%', 
+  },
   buttons:{
     display: 'flex',
     flexDirection: 'row',
     justifyContent: 'space-evenly',
     alignItems: 'center',
     maxWidth: '100%',
-
+    
   },
   buttonText: {
     color: 'white', 
     fontSize: 22,
+  },
+  
+  calendarButtonTextBottom: {
+    color: 'black', 
+    fontSize: 18,
+    marginTop: 5,
+    marginRight: 130,
+  },
+  calendarButtonTextTop: {
+    color: 'black', 
+    fontSize: 22,
+    fontWeight: 700,
+    marginRight: 200,
   },
   calendarModal: {
    flex: 1,
@@ -327,16 +361,37 @@ const styles = StyleSheet.create({
   calendar: {
     borderRadius: 10,
     elevation: 4,
-    width: '100%', // Set the width of the calendar to 100%
+    width: '90%', // Set the width of the calendar to 90%
     maxWidth: 400, // Set a maximum width for the calendar
     maxHeight: 600, // Set a maximum height for the calendar
+    alignSelf: 'center', // Center the calendar horizontally
+    justifyContent: 'center', // Center the calendar vertically
+    marginTop: 25,
+  
+    // Android shadow properties
+    elevation: 2, // Lower than before to create a lighter shadow
+  
+    // iOS shadow properties
+    shadowColor: "#000",
+		shadowOffset: {
+			width: 2,
+			height: 2,
+		},
+		shadowOpacity: 0.11,
+		shadowRadius: 5,
   },
   titleText: {
-    color: 'tan',
-    fontWeight: 800,
-    fontSize: 15,
-    marginLeft: 5,
-    
+    fontWeight: 700,
+    fontSize: 25,
+    marginLeft: 25, // There were two marginLeft properties, I left the larger one
+    color: 'black',
+    marginTop: 30,
+    marginBottom: 10,
+  
+    // Text shadow properties
+    textShadowColor: 'rgba(0, 0, 0, 0.1)', // Very light shadow
+    textShadowOffset: { width: -1, height: 1 }, // Offset for shadow
+    textShadowRadius: 10, // Softness of shadow edges
   },
   exerciseLogModal: {
   flex: 1,
@@ -422,31 +477,54 @@ const styles = StyleSheet.create({
   shadowOpacity: 0.35,
   shadowRadius: 3.84,
 },
-button3: {
-  backgroundColor: 'tan',
-  borderRadius: 20, // Increase for more rounded corners
-  borderColor: '#D2B48C', // Similar to tan, adjust as needed
-  borderWidth: 1, // Set to create a border around the button
-  margin: 40,
+viewERButton: {
+  backgroundColor: 'white', // if you haven't defined it already
+  borderColor: "black",
+  borderWidth: 0, // No border width
+  borderRadius: 20,
   padding: 10,
-  width: 110,
+  width: 340,
+  marginTop: 15,
   alignItems: 'center',
-  justifyContent: 'center', // Center the text vertically
+  justifyContent: 'center',
 
   // Android shadow properties
-  elevation: 5,
+  elevation: 10, // Increase for more shadow on Android
+
+  // iOS shadow properties
+  shadowColor: "#000",
+		shadowOffset: {
+			width: 2,
+			height: 2,
+		},
+		shadowOpacity: 0.11,
+		shadowRadius: 5,
+},
+
+insertERButton: {
+  backgroundColor: 'white', // if you haven't defined it already
+  borderColor: "black",
+  borderWidth: 0, // No border width
+  borderRadius: 20,
+  padding: 10,
+  width: 340,
+  marginTop: 30,
+  alignItems: 'center',
+  justifyContent: 'center',
+
+  // Android shadow properties
+  elevation: 10, // Increase for more shadow on Android
 
   // iOS shadow properties
   shadowColor: "#000",
   shadowOffset: {
-    width: 0,
+    width: 2,
     height: 2,
   },
-  shadowOpacity: 0.25,
-  shadowRadius: 3.84,
+  shadowOpacity: 0.11,
+  shadowRadius: 5,
 },
 });
-
 
 export default CalenderScreen;
 
